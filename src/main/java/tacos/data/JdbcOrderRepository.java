1e3dc6d0dcd873157aed57a24deccc9109cbb934
biv <gurikin@yandex.ru>
2021-06-14 10:38:07 +0300

    3.1_JDBC maked JdbcOrderRepository

--- src/main/java/tacos/data/JdbcOrderRepository.java
+++ src/main/java/tacos/data/JdbcOrderRepository.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import tacos.entity.Order;
+import tacos.entity.Taco;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.UUID;
 
 public class JdbcOrderRepository implements OrderRepository {
     private SimpleJdbcInsert orderInserter;
@@ -16,25 +18,35 @@ public class JdbcOrderRepository implements OrderRepository {
 
     @Autowired
     public JdbcOrderRepository(JdbcTemplate jdbcTemplate) {
-        this.orderInserter = new SimpleJdbcInsert(jdbcTemplate)
-                .withTableName("Taco_Order")
+        this.orderInserter = new SimpleJdbcInsert(jdbcTemplate).withTableName("Taco_Order")
                 .usingGeneratedKeyColumns("id");
-        this.orderTacoInserter = new SimpleJdbcInsert(jdbcTemplate)
-                .withTableName("Taco_Order_Tacos");
+        this.orderTacoInserter = new SimpleJdbcInsert(jdbcTemplate).withTableName("Taco_Order_Tacos");
         this.objectMapper = new ObjectMapper();
     }
 
     @Override
     public Order save(Order order) {
         order.setCreatedAt(LocalDateTime.now());
-        Strign orderId = saveOrderDetails(order);
-        return null;
+        String orderId = saveOrderDetails(order);
+        order.setId(orderId);
+        order.getTacos().foreach(taco -> saveTacoToOrder(taco, orderId));
+
+        return order;
     }
 
-    private Strign saveOrderDetails(Order order) {
+    private String saveOrderDetails(Order order) {
         @SuppressWarnings("unchecked")
         Map<String, Object> values = objectMapper.convertValue(order, Map.class);
         values.put("createdAt", order.getCreatedAt());
-        return "";
+        values.put("id", UUID.randomUUID().toString());
+        orderInserter.execute(values);
+        return values.get("id").toString();
+    }
+
+    private void saveTacoToOrder(Taco taco, long orderId) {
+        Map<String, Object> values = new HashMap<>();
+        values.put("tacoOrder", orderId);
+        values.put("taco", taco.getId());
+        orderTacoInserter.execute(values);
     }
 }
