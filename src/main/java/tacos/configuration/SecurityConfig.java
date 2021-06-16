5645b505c8dee5e9cb9f5fe365a5422b2970d48d
biv <gurikin@yandex.ru>
2021-06-16 22:55:08 +0300

    4_secure remove some trash

--- src/main/java/tacos/configuration/SecurityConfig.java
+++ src/main/java/tacos/configuration/SecurityConfig.java
@@ -1,7 +1,5 @@
 package tacos.configuration;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
