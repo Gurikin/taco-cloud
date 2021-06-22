8c7ba23e6401d09824a4fdc34d27c3f57879133e
biv <gurikin@yandex.ru>
2021-06-22 21:29:36 +0300

    5_config_props setup embedded server

--- src/main/java/tacos/configuration/SecurityConfig.java
+++ src/main/java/tacos/configuration/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/");
         http.csrf().disable();
         http.headers().frameOptions().disable();
+        http.requiresChannel().anyRequest().requiresSecure();
     }
 
     @Bean
