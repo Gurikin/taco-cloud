f3d93e83c19ed43b22d1109b46a03203e748d6d7
Harry <gurikin@yandex.ru>
2021-06-29 23:21:45 +0300

    5_config_props added OrderProps

--- src/main/java/tacos/controller/OrderController.java
+++ src/main/java/tacos/controller/OrderController.java
@@ -1,8 +1,10 @@
 package tacos.controller;
 
-import javax.validation.Valid;
-
+import javassist.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,23 +14,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-
-import javassist.NotFoundException;
-import lombok.extern.slf4j.Slf4j;
+import tacos.configuration.OrderProps;
 import tacos.data.OrderRepository;
 import tacos.entity.Order;
 import tacos.entity.User;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
 public class OrderController {
+
     private OrderRepository orderRepository;
+    private OrderProps orderProps;
 
     @Autowired
-    public OrderController(OrderRepository orderRepository) {
+    public OrderController(OrderRepository orderRepository,
+                           OrderProps orderProps) {
         this.orderRepository = orderRepository;
+        this.orderProps = orderProps;
     }
 
     /**
@@ -42,7 +48,6 @@ public class OrderController {
     }
 
     /**
-     *
      * @param order  The Order model
      * @param errors The possible user's errors in the order form
      * @return String
@@ -50,10 +55,10 @@ public class OrderController {
      */
     @PostMapping
     public String processOrder(
-        @Valid Order order,
-        Errors errors,
-        SessionStatus sessionStatus,
-        @AuthenticationPrincipal User user
+            @Valid Order order,
+            Errors errors,
+            SessionStatus sessionStatus,
+            @AuthenticationPrincipal User user
     ) {
         if (errors.hasErrors()) {
             return "orderForm";
@@ -61,9 +66,17 @@ public class OrderController {
 
         order.setUser(user);
         orderRepository.save(order);
-        
+
         sessionStatus.setComplete();
         log.info("Order submitted: " + order);
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, orderProps.getPageSize());
+        model.addAttribute("orders", orderRepository.findByUserOrderByCreatedAtDesc(user, pageable));
+
+        return "orderList";
     }
 }
--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -1,8 +1,13 @@
 package tacos.data;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import tacos.entity.Order;
+import tacos.entity.User;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends BaseRepository<Order> {
+    List<Order> findByUserOrderByCreatedAtDesc(User user, Pageable pageable);
 }
