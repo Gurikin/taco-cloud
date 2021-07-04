5465f94577cef19f7417bd2e6acb25079dab512d
biv <gurikin@yandex.ru>
2021-07-04 21:11:44 +0300

    6-rest

--- src/main/java/tacos/web/api/DesignTacoRestController.java
+++ src/main/java/tacos/web/api/DesignTacoRestController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
 import tacos.data.TacoRepository;
 import tacos.entity.Taco;
 
